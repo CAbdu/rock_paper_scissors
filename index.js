68b0b5409c86ea3078b28b7a0049db3fcfdad3b9
CAbdu <abduchaib@hotmail.fr>
2024-02-17 06:00:37 +0100

    playround

--- index.js
+++ index.js
@@ -5,5 +5,22 @@ function getComputerChoice(choices) {
 }
 
 const choices = ["rock", "paper", "scissors"];
+// const rules = "rock" < "paper"; "rock" > "scissors"; "scissors" > "paper";
 console.log(getComputerChoice(choices));
 
+
+
+function playRound (playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) { 
+    return "its a tie, replay the round" }
+  else if (computerSelection == "scissors") {
+    return " Congrats mate! "}
+  else (computerSelection == "paper"); {
+    return "You lose :(" };
+  
+  
+  }
+    
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice(choices);
+  console.log(playRound(playerSelection, computerSelection));
